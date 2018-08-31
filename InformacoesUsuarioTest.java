cd30bf005243cda2adeff69ae176d5ce8f272e4d
FabianaSobral <34558648+FabianaSobral@users.noreply.github.com>
2018-08-31 20:35:34 -0300

    Update InformacoesUsuarioTest.java

--- InformacoesUsuarioTest.java
+++ InformacoesUsuarioTest.java
@@ -3,10 +3,11 @@ package testes;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import javax.xml.ws.WebEndpoint;
 
 public class InformacoesUsuarioTest {
     @Test
@@ -17,6 +18,19 @@ public class InformacoesUsuarioTest {
 
         //Navegando para a página Taskit
         navegador.get("http://www.juliodelima.com.br/taskit");
+
+        //Clicar no link com texto "Sign In"
+        navegador.findElement(By.linkText("Sign in")).click();
+
+        //Clicar no campo com nome "Login"
+         WebElement formulatioSignInBox = navegador.findElement (By.id("signibox"));
+
+        //Digitar no campo com nome "Login" o texto "julio0001"
+        //Clicar no campo com nome "Password"
+        //Digitar no campo com nome "Password" o texto "123456"
+        //Clicar no link com texto "Sign In"
+        //Validar que dentro do elemento com class "me" está o textp "Hi, Julio"
+        //Fechar o navegador
 
         //Validação
        assertEquals( '1', '1' );
