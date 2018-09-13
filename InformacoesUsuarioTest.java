19d197b580ddca3a804d89308c1e38c60f3268be
FabianaSobral <34558648+FabianaSobral@users.noreply.github.com>
2018-09-13 02:37:38 -0300

    Update InformacoesUsuarioTest.java

--- InformacoesUsuarioTest.java
+++ InformacoesUsuarioTest.java
@@ -8,32 +8,48 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class InformacoesUsuarioTest {
     @Test
     public void testAdcionarUmaInformacaoAdicionalDeUsuario () {
         //Abrindo o navegador
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\Drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Drivers\\Teste\\chromedriver.exe");
         WebDriver navegador = new ChromeDriver();
-
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         //Navegando para a página Taskit
         navegador.get("http://www.juliodelima.com.br/taskit");
 
         //Clicar no link com texto "Sign In"
         navegador.findElement(By.linkText("Sign in")).click();
 
-        //Clicar no campo com nome "Login"
-         WebElement formulatioSignInBox = navegador.findElement (By.id("signibox"));
+        // Identificando o formulario de "Login"
+        WebElement formularioSignInBox =navegador.findElement(By.id("signinbox"));
 
         //Digitar no campo com nome "Login" o texto "julio0001"
-        //Clicar no campo com nome "Password"
+        formularioSignInBox.findElement(By.name("login")).sendKeys("julio0001");
+
         //Digitar no campo com nome "Password" o texto "123456"
+        formularioSignInBox.findElement(By.name("password")).sendKeys("123456");
+
+
         //Clicar no link com texto "Sign In"
+        navegador.findElement(By.linkText("SIGN IN")).click();
+
+
         //Validar que dentro do elemento com class "me" está o textp "Hi, Julio"
+        WebElement me =navegador.findElement(By.className("me"));
+        String textoNoElementoMe = me.getText();
+        assertEquals("Hi, Julio", textoNoElementoMe);
+
         //Fechar o navegador
+        navegador.quit();
 
         //Validação
-       assertEquals( '1', '1' );
+        assertEquals( '1', '1' );
+
+
 
     }
 
